da3a0cb9a4663e3ed77d3aa1e6214733a51bdfce
José Roniérison <ronierison.silva@gmail.com>
2016-01-31 11:09:08 -0300

    Remove testes nativos do rails

--- test/models/analyte_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AnalyteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/sampling_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SamplingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
